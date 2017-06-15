5a81e44fc2ebc8fc733e2fa445a09841bc77175f
Jose Evanan <jose.evanan@gmail.com>
2017-06-15 07:58:06 -0500

    a#

--- app.js
+++ app.js
@@ -2,13 +2,22 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const hbs = require('express-handlebars')
 const app = express()
 const api = require('./routes')
 //const Product = require('./models/product')
 //middleware
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
+app.engine('.hbs', hbs({
+  defaultLayaut: 'default',
+  extname: '.hds',
+}))
+app.set('view engine', '.hbs')
 app.use('/api', api)
+app.use('/login', (req, res) => {
+  res.render('login')
+})
 //app.get('/hola/:name', (req, res) => {
 //	res.send({ message: `Hola ${req.params.name}!`})
 //})
